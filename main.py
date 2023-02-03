acc99875ea5c5ffed95fc2d7aca6a59e43239bd8
Любомир Швецов <lyubomirshvetsov@yandex.ru>
2023-02-03 17:12:18 +0200

    Задача №2

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ from io import BytesIO
 import requests
 
 
+def change_delta(previous, k):
+    f_p = float(previous)
+    change = f_p / 1.6
+    if 50 > f_p + k * change > 0:
+        f_p += k * change
+    return str(f_p)
+
+
 if __name__ == '__main__':
 
     pygame.init()
@@ -17,11 +25,16 @@ if __name__ == '__main__':
     coords = ('37.168', '56.737')  #Изменяемая часть
     delta = '0.002' #Изменяемая часть
 
-
+    FPS = 10
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_PAGEDOWN:
+                    delta = change_delta(delta, -1)
+                elif event.key == pygame.K_PAGEUP:
+                    delta = change_delta(delta, 1)
 
         params = {
             "ll": ",".join([coords[0], coords[1]]),
@@ -37,3 +50,4 @@ if __name__ == '__main__':
         screen.blit(img, (0, 0))
 
         pygame.display.flip()
+        pygame.time.Clock().tick(FPS)
