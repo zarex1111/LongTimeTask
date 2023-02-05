68955979be7bc0d6ca0b8275d8026487062bd1a7
Lyubomir <lyubomirshvetsov@ynadex.com>
2023-02-05 11:07:33 +0200

    Задача №5

--- main.py
+++ main.py
@@ -88,9 +88,12 @@ if __name__ == '__main__':
 
     toponym = ''
     should_search_toponym = False
-    font = pygame.font.Font('font.otf', 30)
+    font = pygame.font.Font('font.otf', 20)
+    button_font = pygame.font.Font(None, 13)
     textinput = TextInputVisualizer(font_object=font)
-    search_button = Button(screen, 550, 500, 50, 50, font=font, onClick=lambda: get_toponym())
+    search_button = Button(screen, 550, 500, 50, 50, font=button_font, onClick=lambda: get_toponym(), text='Поиск')
+
+    marks = []
 
     FPS = 60
     current_map_value = 0
@@ -115,18 +118,28 @@ if __name__ == '__main__':
                     k2 = 1
         coords = change_coords(coords, k1, k2, delta)
 
-        params = {
-            "ll": ",".join([coords[0], coords[1]]),
-            "spn": ",".join([delta, delta]),
-            "l": MAP_VALUES[current_map_value]
-        }
-
-        pygame_widgets.update(events)
         textinput.update(events)
 
         if should_search_toponym and toponym != '':
             params = get_parameters(toponym, delta)
             params['pt'] = params['ll'] + ',ya_ru'
+            should_search_toponym = False
+            delta = params['spn'].split(',')[0]
+            coords = params['ll'].split(',')
+            marks = [coords]
+
+        params = {
+            "ll": ",".join([coords[0], coords[1]]),
+            "spn": ",".join([delta, delta]),
+            "l": MAP_VALUES[current_map_value],
+            'pt': ''
+        }
+        for mark in marks:
+            params['pt'] = ','.join(mark) + ',ya_ru'
+            if len(marks) > 1:
+                params['pt'] += '&'
+        if len(params['pt']) == 0:
+            params.pop('pt')
 
         map_api_server = "http://static-maps.yandex.ru/1.x/"
         response = requests.get(map_api_server, params=params)
@@ -136,6 +149,7 @@ if __name__ == '__main__':
         screen.blit(img, (0, 50))
         
         screen.blit(textinput.surface, (0, 510))
+        pygame_widgets.update(events)
         pygame.display.update()
         pygame.display.flip()
         pygame.time.Clock().tick(FPS)
